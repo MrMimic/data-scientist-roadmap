a50a1159df851377292551693a7ebd99200a0295
MrMimic <emeric.dynomant@gmail.com>
2017-06-05 09:34:25 +0200

    1_Python-basics.py

---
+++ 3_Programming/1_python-basics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+''' 1_ Python basics '''
+
+# Print something 
+print('Hello, world')
+
+# Assign a variable
+a = 23
+b = 'Hi guys, i\'m a text variable'
+print('This is my variable: {}'.format(b))
+
+# Mathematics
+c = (a + 2) * (245 / 23)
+print('This is mathe-magic: {}'.format(c))
+
