3507ed569d8d4ee50df27461455622e49fcb6b08
Eva-Kaushik <146417148+Eva-Kaushik@users.noreply.github.com>
2024-04-20 14:07:15 +0530

    Update 1_data-exploration.R
    Changed
    
    Co-Authored-By: Eva Kaushik <56021884+eva-spec@users.noreply.github.com>

--- 06_Data-Visualization/1_data-exploration.R
+++ 06_Data-Visualization/1_data-exploration.R
@@ -1,25 +1,23 @@
-#####################
-# To execute line by line in Rstudio, select it (hightlight)
-# Press Ctrl+Enter
+# Load the plotly library for interactive visualization
+library(plotly)
 
-# Iris is an array of values examples coming with R.
+# Iris is a built-in dataset in R
 data <- iris
-# This is equal to :
-data = iris
-# To print it: Sepal.Length Sepal.Width Petal.Length Petal.Width    Species
-show(data)
+
+# Print the dataset
+print(data)
 
 # Histogram
-column = data[,1]
-hist(column)
-# Change parameters :
-hist(column, main = "Main title", xlab = "SEPAL LENGTH", ylab = "FREQUENCY", col = 'red', breaks = 10)
-hist(column, main = "Main title", xlab = "SEPAL LENGTH", ylab = "FREQUENCY", col = 'red', breaks = 15)
+column <- data[,1]
+hist(column, main = "Histogram of Sepal Length", xlab = "Sepal Length", ylab = "Frequency", col = 'skyblue', border = 'white')
 
 # Box plot
-boxplot(column, main = "Main title", ylab = "SEPAL LENGTH", col = 'red')
+boxplot(column, main = "Boxplot of Sepal Length", ylab = "Sepal Length", col = 'salmon')
 
-# Line chart, not very useful here, indeed
-X = data[,1]
-Y = data[,3]
-plot(x = X, y = Y, main = "Main title", xlab = "SEPAL LENGTH", ylab = "PETAL LENGTH", col = 'red')
+# Line chart
+X <- data[,1]
+Y <- data[,3]
+plot_ly(x = X, y = Y, type = "scatter", mode = "markers", marker = list(color = "red")) %>%
+  layout(title = "Line Chart of Sepal Length vs. Petal Length",
+         xaxis = list(title = "Sepal Length"),
+         yaxis = list(title = "Petal Length"))
